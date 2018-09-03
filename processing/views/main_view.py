07d1e6da7baec780d49d23eeb14af61fe7e1799e
haykhman <haykhman@gmail.com>
2018-09-03 13:09:09 +0400

    main page redirect

--- processing/views/main_view.py
+++ processing/views/main_view.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect
 from django.views.generic import View
-from django.utils.translation import get_language
 
 class EnteryLanguageDetect(View):
     def get(self, request, *args, **kwargs):
-        return redirect('/' + get_language())
+        return redirect('/' + request.LANGUAGE_CODE)
